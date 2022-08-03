4aac0bf2073f358467ab8cbd5230cf79cd03fa0d
rikiarizona <rikiarizona@gmail.com>
2022-08-03 09:31:24 +0700

    TUGAS 4 JAVASCRIPT
    saya kirimkan tugas kepada niomic

---
+++ index.js
@@ -0,0 +1,24 @@
+function tinggisiswa() {
+    var riko = 175
+    var budi = 170
+    var riska = 165
+
+    if(riko>budi){
+        if(riko>riska){
+            console.log("Siswa Tertinggi")
+        }else{
+            console.log("Siswa Tertinggi Kedua")
+        }
+    }else{
+        if(riko<riska){
+            console.log("Siswa Terpendek")
+        }else{
+            console.log("Siswa Terpendek Kedua")
+        }
+    }
+    console.log(riko)
+    console.log(budi)
+    console.log(riska)
+}
+
+tinggisiswa()
